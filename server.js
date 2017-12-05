3609919517f7675c161c523a9e09130d798287ee
riley napier <rileylnapier@rileys-MacBook-Pro.local>
2017-12-05 10:17:09 -0800

    style(Fixing flow and making sure to return ReportChunks to server.js):

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 module.exports = {
   flushModuleIds: require('./dist/requireUniversalModule').flushModuleIds,
   flushChunkNames: require('./dist/requireUniversalModule').flushChunkNames,
-  clearChunks: require('./dist/requireUniversalModule').clearChunks
+  clearChunks: require('./dist/requireUniversalModule').clearChunks,
+  ReportChunks: require('./dist/report-chunks').default
 }
--- src/report-chunks.js
+++ src/report-chunks.js
@@ -1,7 +1,14 @@
+// @flow
+
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default class ReportChunks extends React.Component {
+type Props = {
+  report: Function,
+  children: Object
+}
+
+export default class ReportChunks extends React.Component<void, Props, *> {
   static propTypes = {
     report: PropTypes.func.isRequired
   }
